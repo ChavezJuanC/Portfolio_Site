a3c4612dbb522ff2a19c360e3c8c7b25141537d2
SKRPTZ <43616880+ChavezJuanC@users.noreply.github.com>
2025-04-19 01:04:05 -0600

    Linked github

--- src/routes/navigation/Footer.jsx
+++ src/routes/navigation/Footer.jsx
@@ -5,7 +5,9 @@ function Footer() {
     return (
         <div className="h-10 bg-[#4A4A4A] mt-22 text-[#FFFFFF] flex justify-end items-center w-full">
             <div className="flex flex-row mr-5">
-                <FaGithub className="w-5 h-5 mx-2 hover:cursor-pointer" />
+                <a href="https://github.com/ChavezJuanC">
+                    <FaGithub className="w-5 h-5 mx-2 hover:cursor-pointer" />
+                </a>
                 <FaLinkedin className="w-5 h-5 mx-2 hover:cursor-pointer" />
                 <FaYoutube className="w-5 h-5 mx-2 hover:cursor-pointer" />
                 <FaItchIo className="w-5 h-5 mx-2 hover:cursor-pointer" />
